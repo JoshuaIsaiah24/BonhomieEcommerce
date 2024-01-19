8c88d6c69b99680a1db6c60cdb3d8cc259f13f09
JoshuaIsaiah24 <143246779+JoshuaIsaiah24@users.noreply.github.com>
2024-01-19 17:18:09 +0800

    Fixing Shop page and dynamic features from Django

--- Bonhomie/store/views.py
+++ Bonhomie/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Tag, Brand, Product
 
 # Create your views here.
 
@@ -9,4 +10,25 @@ def about(request):
     return render(request, 'about.html', {})
 
 def shop(request):
-    return render(request, 'shop.html', {})
+    products = Product.objects.all()
+    tags = Tag.objects.all()
+    brands = Brand.objects.all()
+    
+    # Filtering by tag
+    tag_filter = request.GET.get('tag')
+    if tag_filter:
+        products = products.filter(tags__name=tag_filter)
+
+    # Filtering by brand
+    brand_filter = request.GET.get('brand')
+    if brand_filter:
+        products = products.filter(brand__name=brand_filter)
+
+    # Sorting by price
+    sort_option = request.GET.get('sort')
+    if sort_option == 'low_to_high':
+        products = products.order_by('price')
+    elif sort_option == 'high_to_low':
+        products = products.order_by('-price')
+        
+    return render(request, 'shop.html', {'products': products, 'tags': tags, 'brands': brands})
