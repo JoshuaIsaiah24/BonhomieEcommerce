db48b616b5048b04b4bbf14fa1635121b83cfcf2
JoshuaIsaiah24 <143246779+JoshuaIsaiah24@users.noreply.github.com>
2024-01-22 16:29:20 +0800

    successfully added product filter by price range, tags, brands

--- Bonhomie/static/js/script.js
+++ Bonhomie/static/js/script.js
@@ -50,23 +50,30 @@
   }
 
   // Tab Section
-  var initTabs = function() {
+  var initTabs = function () {
     const tabs = document.querySelectorAll('[data-tab-target]')
     const tabContents = document.querySelectorAll('[data-tab-content]')
 
     tabs.forEach(tab => {
       tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-          tabContent.classList.remove('active')
-        })
-        tabs.forEach(tab => {
-          tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-        target.classList.add('active')
+
+        if (target) {
+          tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+          })
+
+          tabs.forEach(tab => {
+            tab.classList.remove('active')
+          })
+
+          tab.classList.add('active')
+          target.classList.add('active')
+        } else {
+          console.error('Target element not found!')
+        }
       })
-    });
+    })
   }
 
   // document ready
--- Bonhomie/store/views.py
+++ Bonhomie/store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Tag, Brand, Product
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Tag, Brand, Product
 
 # Create your views here.
 
@@ -9,26 +9,45 @@ def home(request):
 def about(request):
     return render(request, 'about.html', {})
 
-def shop(request):
+def shop(request, category_slug=None):
+    # Get all products, categories, tags, and brands initially
     products = Product.objects.all()
+    categories = Category.objects.all()
     tags = Tag.objects.all()
     brands = Brand.objects.all()
-    
-    # Filtering by tag
-    tag_filter = request.GET.get('tag')
+
+    # Filtering by Category
+    if category_slug and category_slug != 'all':
+        products = products.filter(category__slug=category_slug)
+
+    # Create the initial context dictionary
+    context = {
+        'categories': categories,
+        'products': products,
+        'selected_category': category_slug,
+        'tags': tags,
+        'brands': brands,
+    }
+
+    # Filtering by Tag
+    tag_filter = request.GET.get('tags')
     if tag_filter:
         products = products.filter(tags__name=tag_filter)
 
-    # Filtering by brand
+    # Filtering by Brand
     brand_filter = request.GET.get('brand')
     if brand_filter:
         products = products.filter(brand__name=brand_filter)
 
-    # Sorting by price
-    sort_option = request.GET.get('sort')
-    if sort_option == 'low_to_high':
-        products = products.order_by('price')
-    elif sort_option == 'high_to_low':
-        products = products.order_by('-price')
-        
-    return render(request, 'shop.html', {'products': products, 'tags': tags, 'brands': brands})
+    # Sorting by Price
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    # Filter products based on the price range
+    if min_price and max_price:
+        products = products.filter(price__gte=min_price, price__lte=max_price)
+
+    # Update the context dictionary with the filtered products
+    context['products'] = products
+
+    return render(request, 'shop.html', context)
