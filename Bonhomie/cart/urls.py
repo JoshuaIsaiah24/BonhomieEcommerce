70802c5922301ed1eeab067d17bfe02ad444fb2c
JoshuaIsaiah24 <143246779+JoshuaIsaiah24@users.noreply.github.com>
2024-02-16 19:06:32 +0800

    still working on the cart page

--- Bonhomie/cart/urls.py
+++ Bonhomie/cart/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart-details', views.cart_details, name='cart'),
+    path('', views.cart_details, name='cart'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
